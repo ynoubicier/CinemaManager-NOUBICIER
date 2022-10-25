1971941775e86283c1f97b34742ec51441cda3b0
yvan yanova <yvanyanova75@outlook.fr>
2022-10-25 02:57:51 +0100

    Ajoutez des fichiers projet.

---
+++ CinemaManager-NOUBICIER/Models/Cinema/CinemaDBEContext.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace CinemaManager_NOUBICIER.Models.Cinema
+{
+    public partial class CinemaDBEContext : DbContext
+    {
+        public CinemaDBEContext()
+        {
+        }
+
+        public CinemaDBEContext(DbContextOptions<CinemaDBEContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Movie> Movies { get; set; } = null!;
+        public virtual DbSet<Producer> Producers { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Name=ConnectionStrings:CinemaCS");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Movie>(entity =>
+            {
+                entity.ToTable("Movie");
+
+                entity.Property(e => e.Genre).HasMaxLength(30);
+
+                entity.Property(e => e.Title).HasMaxLength(30);
+
+                entity.HasOne(d => d.Producer)
+                    .WithMany(p => p.Movies)
+                    .HasForeignKey(d => d.ProducerId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Movie_Producer");
+            });
+
+            modelBuilder.Entity<Producer>(entity =>
+            {
+                entity.ToTable("Producer");
+
+                entity.Property(e => e.Email).HasMaxLength(30);
+
+                entity.Property(e => e.Name).HasMaxLength(30);
+
+                entity.Property(e => e.Nationality).HasMaxLength(30);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ CinemaManager-NOUBICIER/Models/Cinema/Producer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CinemaManager_NOUBICIER.Models.Cinema
+{
+    public partial class Producer
+    {
+        public Producer()
+        {
+            Movies = new HashSet<Movie>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Nationality { get; set; } = null!;
+        public string Email { get; set; } = null!;
+
+        public virtual ICollection<Movie> Movies { get; set; }
+    }
+}
